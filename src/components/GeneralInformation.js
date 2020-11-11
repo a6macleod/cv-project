68606337bac4882fc00b9914a09f2f33d93ed837
Andrew MacLeod <A6macleod@gmail.com>
2020-11-10 20:30:48 -0500

    Update display of the General Info section

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -4,12 +4,13 @@ const GeneralInformation = (props) => {
   // enter education info
   return (
     <div className="mt-2 mb-5">
-      <div className="wrapper">
-        <h1 className="name">{props.name}</h1>
-      </div>
       <div className="row justify-content-center">
-        <div className="card col-md- d-flex justify-content-center mt-3 p-2">
+        <div className="card col-lg- d-flex justify-content-center mt-3 p-2">
           <h2>Contact Information</h2>
+          <div className="wrapper d-flex justify-content-center align-items-center">
+            <h4 className="mr-3 text-muted">Name:</h4>
+            <h3 className="name">{props.name}</h3>
+          </div>
           <div className="wrapper d-flex justify-content-center align-items-center">
             <h4 className="mr-3 text-muted">Email:</h4>
             <h3 className="email">{props.email}</h3>
