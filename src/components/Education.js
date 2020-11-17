b06dbc6af624f3a063815c5be550a9b4995cf1c4
Andrew MacLeod <A6macleod@gmail.com>
2020-11-16 20:46:24 -0500

    Add hooks to Education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 
+/* education: [
+  {
+    school: "Western Michigan University",
+    degree: "Bachelor of Science",
+    focus: "Geochemistry",
+    yearGraduated: "2010",
+  },
+  {
+    school: "Western Michigan University",
+    degree: "Master of Science",
+    focus: "Geochemistry",
+    yearGraduated: "2013",
+  },
+], */
+
 const Education = (props) => {
+  const [schoolOne, setSchoolOne] = useState("Western Michigan University");
+  const [degreeOne, setDegreeOne] = useState("Bachelor of Science");
+  const [focusOne, setFocusOne] = useState("Geochemistry");
+  const [yearGraduatedOne, setYearGraduatedOne] = useState(2010);
+  const [schoolTwo, setSchooTwo] = useState("Western Michigan University");
+  const [degreeTwo, setDegreeTwo] = useState("Master of Science");
+  const [focusTwo, setFocusTwo] = useState("Geochemistry");
+  const [yearGraduatedTwo, setYearGraduatedTwo] = useState(2013);
+
   return (
     <div className="mb-5">
       <h2>Education Experience</h2>
       <div className="row justify-content-center">
-        {props.education.map((edu, index) => {
-          return (
-            <div key={uniqid()} className="col-md- wrapper card p-2 m-2">
-              <h3 className="school">{edu.school}</h3>
-              <h4 className="degree">{edu.degree}</h4>
-              <h4 className="Focus">{edu.focus}</h4>
-              <h4 className="yearGraduated">{edu.yearGraduated}</h4>
-            </div>
-          );
-        })}
+        <div key={uniqid()} className="col-md- wrapper card p-2 m-2">
+          <h3 className="school">{schoolOne}</h3>
+          <h4 className="degree">{degreeOne}</h4>
+          <h4 className="Focus">{focusOne}</h4>
+          <h4 className="yearGraduated">{yearGraduatedOne}</h4>
+        </div>
+        <div key={uniqid()} className="col-md- wrapper card p-2 m-2">
+          <h3 className="school">{schoolTwo}</h3>
+          <h4 className="degree">{degreeTwo}</h4>
+          <h4 className="Focus">{focusTwo}</h4>
+          <h4 className="yearGraduated">{yearGraduatedTwo}</h4>
+        </div>
       </div>
       {/* end /.row */}
     </div>
